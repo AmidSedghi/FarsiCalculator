c8715be9a351b330a0874808cdd23f1c07a0f50e
asedghi <asedghi@sfu.ca>
2017-06-13 21:19:00 -0700

    Some new features

--- Farsi Calculator/ViewController.swift
+++ Farsi Calculator/ViewController.swift
@@ -22,11 +22,11 @@ class ViewController: UIViewController {
     
     var labelString: String = "0"
     var currntMode:modes = .not_set
-    var savedNum: Int = 0 // int value before mode button was pressed
+    var savedNum: Double = 0 // int value before mode button was pressed
     var lastButtonWasMode:Bool = false
+    var decimalInserted:Bool = false
+    //var valueNegative:Bool = false
     
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -53,10 +53,12 @@ class ViewController: UIViewController {
         changeMode(newMode: .division)
     }
     
-    
+    /*  Performs the operation based on the mode
+     *  Keep track of negative values
+     */
     @IBAction func didPressEqual(_ sender: AnyObject) {
         // convert string to int
-        guard let labelInt:Int = Int(labelString) else {
+        guard let labelInt:Double = Double(labelString) else {
             return
         }
         // if no mode is selected, nothing to calculate thus return
@@ -64,7 +66,7 @@ class ViewController: UIViewController {
             return
         }
         
-        if (currntMode == .addition){ // performing addition
+        if (currntMode == .addition){
             savedNum += labelInt
         }
         
@@ -77,14 +79,14 @@ class ViewController: UIViewController {
         }
         
         else if (currntMode == .division){
-            savedNum /= labelInt
+            savedNum = savedNum/labelInt
         }
         
         currntMode = .not_set
         labelString = "\(savedNum)"
         updateText()
         lastButtonWasMode = true
-        
+        //valueNegative = false
     }
     
     /*  Clearing the results
@@ -95,6 +97,8 @@ class ViewController: UIViewController {
         currntMode = .not_set
         savedNum = 0
         lastButtonWasMode = false
+        decimalInserted = false
+        //valueNegative = false
         result.text = "0"
     }
     
@@ -107,32 +111,137 @@ class ViewController: UIViewController {
         
         if (lastButtonWasMode){
             lastButtonWasMode = false
-            labelString = "0"
+            if (labelString == "-0"){
+                // do nothing
+            } else {
+                labelString = "0"
+            }
         }
         
         labelString = labelString.appending(stringValue!)
         updateText()
     }
     
-    
-    
-    func updateText(){
-        // turn string to int
-        guard let labelInt:Int = Int(labelString) else {
+    /*  Entering a decimal value:
+     *  ensure +/- button works with . button
+     */
+    @IBAction func didPressDecimal(_ sender: Any) {
+        if (decimalInserted){
             return
         }
+        if (lastButtonWasMode){
+            lastButtonWasMode = false
+            if (labelString == "-0"){
+                //
+            } else {
+                labelString = "0"
+            }
+        }
+        labelString = labelString + "."
+        decimalInserted = true
+        updateText()
+    }
+    
+    /*  Providing sign of the numerical value
+     *  - or + would indicate a different result on operation
+     */
+    @IBAction func didPressSign(_ sender: UIButton) {
+        
+        // if last button was not a mode indicator
+        if (!lastButtonWasMode){
+            
+            print("Hello")
+            // user indicates value will be negative initially
+            if (labelString == "0"){
+                labelString = "-0"
+                //valueNegative = true
+            } else if (labelString == "-0"){
+                labelString = "0"
+                //valueNegative = false
+            }
+            
+            // user indicates value will include decimals
+            if (labelString == "0."){
+                labelString = "-0."
+                //valueNegative = true
+            } else if (labelString == "-0."){
+                labelString = "0."
+                //valueNegative = false
+            }
+
+            // if value is greater than 0, add - sign
+            if (Double(labelString)! > 0.0){
+                labelString = "-" + labelString
+            }
+            // if value is less than zero, get rid of - sign
+            else if (Double(labelString)! < 0.0){
+                var pos:Double = Double(labelString)!
+                pos = -1*pos
+                labelString = String(pos)
+            }
+            
+            updateText()
+        }
+        // if last button is a mode indicator
+        else {
+            labelString = "-0"
+            //valueNegative = true
+            updateText()
+        }
+        
+    }
+    
+    /*  Percent sign, divides by 100
+     *
+     */
+    @IBAction func didPressPercent(_ sender: Any) {
+        
+        var percent:Double = Double(labelString)!
+        percent = percent/100.0
+        labelString = String(percent)
+        
+        updateText()
+    }
+    
+    /*  Update value on display
+     *  append a '.' if decimal is clicked
+     */
+    func updateText(){
+        // turn string to int
+        guard let labelInt:Double = Double(labelString) else {
+            return
+        }
+        print("Updating Text")
+        print("labelString",labelString)
+        print("labelInt", labelInt)
         
         if (currntMode == .not_set){
-            savedNum = labelInt
+            savedNum = Double(labelInt)
         }
         
         // createa number formatter to have decimal style
-        let formatter:NumberFormatter = NumberFormatter() // constructor method 
-        formatter.numberStyle = .decimal
-        let num:NSNumber = NSNumber(value: labelInt)
+        let formatter:NumberFormatter = NumberFormatter() // constructor method
         
+        formatter.maximumFractionDigits = 5
+        
+        if (labelInt != 0.0 && (labelInt > 100000.0)){
+            formatter.numberStyle = .scientific
+        }
+        else {
+            formatter.numberStyle = .decimal
+        }
+        
+        let num:NSDecimalNumber = NSDecimalNumber(value: labelInt)
+        print("num", num)
         // take care of leading zeros aka 00001 is 1
         result.text = formatter.string(from: num)
+ 
+        if (labelString.characters.last == "."){
+            print(".")
+            result.text? = (result.text?.appending("."))!
+        } else if (labelString == ""){
+            result.text? = (result.text?.appending("0"))!
+        }
     }
     
     func changeMode(newMode:modes){
@@ -143,8 +252,7 @@ class ViewController: UIViewController {
         
         currntMode = newMode
         lastButtonWasMode = true
+        decimalInserted = false
     }
-
-
 }
 
